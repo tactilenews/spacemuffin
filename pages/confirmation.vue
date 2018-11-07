e6887d1abdde4679270ddbc277730e03eb890393
Grzegorz Leoniec <greg@app-interactive.de>
2018-11-07 10:24:21 +0100

    added confirmation page and update review page

---
+++ pages/confirmation.vue
@@ -0,0 +1,14 @@
+<template>
+  <ds-container>
+    <ds-card>
+      <ds-heading tag="h3">Erfolgreich</ds-heading>
+      <p>Die Production wurde erfolgreich gestartet</p>
+      <div style="text-align: right">
+        <ds-button
+          icon-right="list"
+          path="/"
+          primary>Zur Übersicht</ds-button>
+      </div>
+    </ds-card>
+  </ds-container>
+</template>
--- pages/review.vue
+++ pages/review.vue
@@ -2,9 +2,11 @@
   <ds-container>
     <ds-card>
       <ds-heading tag="h3">Review</ds-heading>
+      <p>Bitte überprüfe deine Daten</p>
       <div style="text-align: right">
         <ds-button
           icon-right="check"
+          path="/confirmation"
           primary>Produktion starten</ds-button>
       </div>
     </ds-card>
