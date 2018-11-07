06ff7fbf1614c93b5178fce9cfa285c102603c5f
Till Prochaska <mail@tillprochaska.de>
2018-11-07 16:40:53 +0100

    Add header component

---
+++ components/TactileHeader.vue
@@ -0,0 +1,45 @@
+<template>
+  <header>
+    <div class="wrapper">
+      <a href="#">
+        <img
+          src="~assets/img/logo.svg"
+          alt="Tactile News"
+        >
+      </a>
+      <tactile-nav />
+    </div>
+  </header>
+</template>
+
+<script>
+import TactileNav from './TactileNav.vue'
+export default {
+  components: {
+    TactileNav
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import '~assets/styles/variables';
+
+header {
+  padding: 0.5 * $spacing-unit $spacing-unit;
+  background: #fff;
+}
+
+.wrapper {
+  display: flex;
+  margin: auto;
+  max-width: $grid-max-width;
+}
+
+a {
+  margin-right: auto;
+}
+
+img {
+  height: 1.5 * $spacing-unit;
+}
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,30 +1,29 @@
 <template>
   <div>
-    <nav>
-      <img
-        id="logo"
-        src="~assets/img/tactilenews.svg"
-        alt="tactile.news">
-    </nav>
-    <ul>
-      <li>
-        <a href="/">Dashboard</a>
-      </li>
-      <li>
-        <a href="/create">Create</a>
-      </li>
-    </ul>
-    <div style="padding: 3rem 2rem 5rem;">
-      <nuxt/>
-    </div>
+    <tactile-header />
+    <main>
+      <nuxt />
+    </main>
   </div>
 </template>
 
 <script>
+import TactileHeader from '~/components/TactileHeader.vue'
+export default {
+  components: {
+    TactileHeader
+  }
+}
 </script>
 
-<style>
-#logo {
-  height: 50px;
+<style lang="scss" scoped>
+@import '~assets/styles/variables';
+
+main {
+  max-width: $grid-max-width;
+  margin: $spacing-unit auto;
+  padding: $spacing-unit;
+
+  background: #fff;
 }
 </style>
