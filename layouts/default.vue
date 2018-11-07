56442525cff10f1002d969b8d09359d5f28a4b94
Grzegorz Leoniec <greg@app-interactive.de>
2018-11-07 12:42:01 +0100

    added logo and quick navigation

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,30 @@
 <template>
-  <div style="padding: 3rem 2rem 5rem;">
-    <nuxt/>
+  <div>
+    <nav>
+      <img
+        id="logo"
+        src="~assets/img/tactilenews.svg"
+        alt="tactile.news">
+    </nav>
+    <ul>
+      <li>
+        <a href="/">Dashboard</a>
+      </li>
+      <li>
+        <a href="/create">Create</a>
+      </li>
+    </ul>
+    <div style="padding: 3rem 2rem 5rem;">
+      <nuxt/>
+    </div>
   </div>
 </template>
 
 <script>
 </script>
+
+<style>
+#logo {
+  height: 50px;
+}
+</style>
