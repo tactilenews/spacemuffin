aff65216664d774d9045b4028ea4e5912a126ef1
The Amazing PT <theamazingpt@protonmail.com>
2018-11-07 12:55:47 +0100

    feat(confirmation): add basic markup

--- pages/confirmation.vue
+++ pages/confirmation.vue
@@ -1,9 +1,37 @@
 <template>
-  <div>
-    <h2>Erfolgreich</h2>
-    <p>Die Production wurde erfolgreich gestartet</p>
-    <div style="text-align: right">
-      <button>Zur Übersicht</button>
-    </div>
-  </div>
+  <main>
+    <section>
+      <h1>Manuskript Vorschau</h1>
+      <pre>Lorem Ipsum</pre>
+      <div>
+        <span>Titel</span>
+        <span>Der Pferdelüster</span>
+      </div>
+      <div>
+        <span>Autor</span>
+        <span>Ralph Caspers</span>
+      </div>
+      <div>
+        <span>Preis</span>
+        <span>152 Euro</span>
+      </div>
+      <div>
+        <span>Länge</span>
+        <span>2:30 Minuten</span>
+      </div>
+    </section>
+    <section>
+      <div>
+        Ich brauche es bis <input type="date">
+      </div>
+      <div>
+        <h1>Möchtest du dem Sprecher was sagen?</h1>
+        <textarea/>
+      </div>
+      <div>
+        <button>Anfragen</button>
+        <button>Für später sichern</button>
+      </div>
+    </section>
+  </main>
 </template>
