097c3546b9f2c712e274be70ed7b37a13e23a3cd
The Amazing PT <theamazingpt@protonmail.com>
2018-11-08 12:00:25 +0100

    feat(success): add basic success page layout

---
+++ pages/success.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <header>
+      <h1>Herzlichen Glückwunsch!</h1>
+      <p>Dein Beitrag wurde an $SPRECHER versandt. In $TAGEN Tagen sollte alles eingesprochen worden sein.</p>
+    </header>
+    <main>
+      <section class="muffin">
+        <img
+          src="~assets/img/muffin.svg"
+          alt="Pinkes tactile.news Maskottchen"
+        >
+      </section>
+      <section>
+        <h2>Kontaktinfos</h2>
+        Bei Fragen und Anregung kannst du dich gerne bei uns melden:
+        <a href="mailto://support@tactile.news">support@tactile.news</a>
+      </section>
+    </main>
+    <footer>
+      <nuxt-link
+        to="/"
+        tag="button">Zum Dashboard</nuxt-link>
+    </footer>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+@import '~assets/styles/variables';
+
+main {
+  display: flex;
+  justify-content: space-between;
+  flex-direction: column;
+}
+
+footer {
+  margin-top: $spacing-medium;
+  display: flex;
+  justify-content: flex-end;
+}
+
+.muffin {
+  display: flex;
+  justify-content: center;
+}
+</style>
