2c87b75703b5b80b54352a6893dbe030f9fe8cf5
The Amazing PT <theamazingpt@protonmail.com>
2018-11-09 12:15:35 +0100

    fix(index): remove test component

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,5 @@
 <template>
   <tactile-content>
-    <tactile-dropzone />
     <tactile-empty-state to="/create">
       Du hast noch keine Beiträge angelegt. Willst du jetzt anfangen?
       <template slot="action">Beitrag erstellen</template>
@@ -11,12 +10,10 @@
 <script>
 import TactileContent from '~/components/TactileContent.vue'
 import TactileEmptyState from '~/components/TactileEmptyState.vue'
-import TactileDropzone from '~/components/TactileDropzone.vue'
 export default {
   components: {
     TactileContent,
-    TactileEmptyState,
-    TactileDropzone
+    TactileEmptyState
   }
 }
 </script>
