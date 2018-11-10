f4711fc9f60c6cdbb9e308dbecc49d1a7edbfb0d
Till Prochaska <mail@tillprochaska.de>
2018-11-10 18:03:35 +0100

    Fix audio urls

--- store/sounds.js
+++ store/sounds.js
@@ -2,39 +2,39 @@ export const state = () => ({
   speakers: [
     {
       name: 'Basta',
-      url: '/audio/speakers/basta.mp3',
+      url: '/audio/speakers/basta.mp3'
     },
     {
       name: 'Franci',
-      url: '/audio/speakers/franci.mp3',
+      url: '/audio/speakers/franci.mp3'
     },
     {
       name: 'Friedrich',
-      url: '/audio/speakers/friedrich.mp3',
+      url: '/audio/speakers/friedrich.mp3'
     },
     {
       name: 'Giosue Tolu',
-      url: '/audio/speakers/giosue-tolu.mp3',
+      url: '/audio/speakers/giosue-tolu.mp3'
     },
     {
       name: 'Katja',
-      url: '/audio/speakers/katja.mp3',
+      url: '/audio/speakers/katja.mp3'
     },
     {
       name: 'Martin',
-      url: '/audio/speakers/martin.mp3',
+      url: '/audio/speakers/martin.mp3'
     },
     {
       name: 'Matthias',
-      url: '/audio/speakers/matthias.mp3',
+      url: '/audio/speakers/matthias.mp3'
     },
     {
       name: 'Milla',
-      url: '/audio/speakers/milla.mp3',
+      url: '/audio/speakers/milla.mp3'
     },
     {
       name: 'Stefanie',
-      url: '/audio/speakers/stefanie.mp3',
+      url: '/audio/speakers/stefanie.mp3'
     }
   ],
   voices: [
@@ -62,67 +62,67 @@ export const state = () => ({
   sounds: [
     {
       name: 'Baby',
-      url: '/audio/voices/baby.mp3'
+      url: '/audio/sounds/baby.mp3'
     },
     {
       name: 'Kinderlachen',
-      url: '/audio/voices/kinderlachen.mp3'
+      url: '/audio/sounds/kinderlachen.mp3'
     },
     {
       name: 'Kindermusik',
-      url: '/audio/voices/kindermusik.mp3'
+      url: '/audio/sounds/kindermusik.mp3'
     },
     {
       name: 'Knall',
-      url: '/audio/voices/knall.mp3'
+      url: '/audio/sounds/knall.mp3'
     },
     {
       name: 'Percussion',
-      url: '/audio/voices/percussion.mp3'
+      url: '/audio/sounds/percussion.mp3'
     },
     {
       name: 'Pferde',
-      url: '/audio/voices/pferde.wav'
+      url: '/audio/sounds/pferde.wav'
     },
     {
       name: 'Platsch',
-      url: '/audio/voices/platsch.mp3'
+      url: '/audio/sounds/platsch.mp3'
     },
     {
       name: 'Regen',
-      url: '/audio/voices/regen.mp3'
+      url: '/audio/sounds/regen.mp3'
     },
     {
       name: 'Schritte auf Eis',
-      url: '/audio/voices/schritte-auf-eis.mp3'
+      url: '/audio/sounds/schritte-auf-eis.mp3'
     },
     {
       name: 'Schwein',
-      url: '/audio/voices/schwein.mp3'
+      url: '/audio/sounds/schwein.mp3'
     },
     {
       name: 'Schwimmbad',
-      url: '/audio/voices/schwimmbad.mp3'
+      url: '/audio/sounds/schwimmbad.mp3'
     },
     {
       name: 'Spannende Musik',
-      url: '/audio/voices/spannende-musik.mp3'
+      url: '/audio/sounds/spannende-musik.mp3'
     },
     {
       name: 'Spielende Kinder',
-      url: '/audio/voices/spielende-kinder.mp3'
+      url: '/audio/sounds/spielende-kinder.mp3'
     },
     {
       name: 'Stalltür',
-      url: '/audio/voices/stalltür.wav'
+      url: '/audio/sounds/stalltür.wav'
     },
     {
       name: 'Wind',
-      url: '/audio/voices/wind.wav'
+      url: '/audio/sounds/wind.wav'
     },
     {
       name: 'Zeit vergeht',
-      url: '/audio/voices/zeit-vergeht.mp3'
+      url: '/audio/sounds/zeit-vergeht.mp3'
     }
   ]
 })
@@ -130,5 +130,11 @@ export const state = () => ({
 export const getters = {
   speakers(state) {
     return state.speakers
+  },
+  voices(state) {
+    return state.voices
+  },
+  sounds(state) {
+    return state.sounds
   }
 }
