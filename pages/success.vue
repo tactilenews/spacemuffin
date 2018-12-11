5e39e53d67f1673d3d7d40e55ae20964b12453e6
Jakob Vicari <kontakt@jakobvicari.de>
2018-12-11 13:06:21 +0100

    Korrigiert: Autorname-darstellung und Zahl tage auf Succes-Seite

--- pages/success.vue
+++ pages/success.vue
@@ -2,7 +2,7 @@
   <div>
     <tactile-content class="centered">
       <h1>Herzlichen Glückwunsch!</h1>
-      <p>Dein Beitrag wurde an <b>{{ speaker }}</b> versandt. In <b>{{ days }} Tagen</b> ist er fertig produziert. Du bekommst ihn dann per E-Mail zugeschickt.
+      <p>Dein Beitrag wurde an <b>{{ speaker.name }}</b> versandt. In <b>{{ timerange }} Tagen</b> ist er fertig produziert. Du bekommst ihn dann per E-Mail zugeschickt.
       <span v-if="recipientEmail"> Wir schicken die Audio-Datei an <b>{{ recipientEmail }}</b>.</span></p>
 
       <img
