b12837d83699a027eb0c58d63738dfb7f6ecfd73
Till Prochaska <mail@tillprochaska.de>
2019-03-19 00:43:13 +0100

    Fix

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -10,14 +10,20 @@ chmod 600 $HOME/.ssh/deploy_rsa
 
 # Stop Nuxt.js service, the web app will be unavailable for the
 # duration of the deployment.
-ssh -i $HOME/.ssh/deploy_rsa $TARGET_USER@$TARGET_HOST 'supervisorctl stop spacemuffin-daemon'
+ssh \
+  -i $HOME/.ssh/deploy_rsa \
+  $TARGET_USER@$TARGET_HOST \
+  'supervisorctl stop spacemuffin-daemon'
 
 # Upload changes to Uberspace using `rsync`
-rsync
-  -hacz --delete --info=name1,stats2
-  -e "ssh -i $HOME/.ssh/deploy_rsa"
-  ./{.nuxt,api,dist,node_modules,static,nuxt.config.js,package.json}
+rsync \
+  -hacz --delete --info=name1,stats2 \
+  -e "ssh -i $HOME/.ssh/deploy_rsa" \
+  ./{.nuxt,api,dist,node_modules,static,nuxt.config.js,package.json} \
   $TARGET_USER@$TARGET_HOST:/var/www/virtual/$TARGET_USER/app
 
 # Start the Nuxt.js service so that the web app is available again
-ssh -i $HOME/.ssh/deploy_rsa $TARGET_USER@$TARGET_HOST 'supervisorctl start spacemuffin-daemon'
+ssh \
+  -i $HOME/.ssh/deploy_rsa \
+  $TARGET_USER@$TARGET_HOST \
+  'supervisorctl start spacemuffin-daemon'
